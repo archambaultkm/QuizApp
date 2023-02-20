fd201bbc7361ae98620311edcc9a961540a6422b
Kaitlyn <97715354+archambaultkm@users.noreply.github.com>
2023-02-20 14:23:07 -0400

    Fixed file reading and displaying new question

--- app/src/main/java/com/example/quizzapp/QuizActivity.java
+++ app/src/main/java/com/example/quizzapp/QuizActivity.java
@@ -2,8 +2,8 @@ package com.example.quizzapp;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -16,24 +16,28 @@ import java.util.Random;
 
 public class QuizActivity extends Activity {
 
+    private static final String TAG = "QuizActivity";
+
     private TextView tvUserTopic, tvQuestion;
     private Button btnAns1, btnAns2, btnAns3, btnAns4;
 
-    private ArrayList<String> questions, answers;
-    private HashMap<String, String> pairs;
+    private final ArrayList<String> questions = new ArrayList<>();
+    private final ArrayList<String> answers = new ArrayList<>();
+
+    private final HashMap<String, String> pairs = new HashMap<>();
 
     String currentQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d(TAG,"Application started");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        questions = new ArrayList<>();
-        answers = new ArrayList<>();
-
         //read in values from text file and fill arraylists
-        //loadTextFile();
+        loadTextFile();
 
         //link questions and answers
         createHash();
@@ -48,13 +52,13 @@ public class QuizActivity extends Activity {
         btnAns4 = findViewById(R.id.btnA4);
 
         //set onclick listeners
-        btnAns1.setOnClickListener(onButtonClicked);
-        btnAns2.setOnClickListener(onButtonClicked);
-        btnAns3.setOnClickListener(onButtonClicked);
-        btnAns4.setOnClickListener(onButtonClicked);
+//        btnAns1.setOnClickListener(onButtonClicked);
+//        btnAns2.setOnClickListener(onButtonClicked);
+//        btnAns3.setOnClickListener(onButtonClicked);
+//        btnAns4.setOnClickListener(onButtonClicked);
 
         //fill tvs and buttons with values
-        //newQuestion();
+        newQuestion();
 
     }//end oncreate
 
@@ -81,42 +85,51 @@ public class QuizActivity extends Activity {
     private void loadTextFile() {
 
         InputStream inputStream;
-        InputStreamReader inputStreamReader;
-        BufferedReader reader;
+        BufferedReader reader = null;
 
         try {
             inputStream = getAssets().open("TestQandAs.txt");
-            inputStreamReader = new InputStreamReader(inputStream);
-            reader = new BufferedReader(inputStreamReader);
 
-            //while there are still lines to read, add q/a pairs
-            String line = "";
-            //String delimiter = "$$";
-            while ((line = reader.readLine()) != null) {
+            if (inputStream != null) {
 
-                String[] pair = line.split(",", 1);
+                reader = new BufferedReader(new InputStreamReader(inputStream));
 
-                questions.add(pair[0]);
-                answers.add(pair[1]);
+                //while there are still lines to read, add q/a pairs
+                String line;
+                String[] pair;
+
+                while ((line = reader.readLine()) != null) {
+
+                    pair = line.split("\\$\\$");
+
+                    questions.add(pair[0]);
+                    answers.add(pair[1]);
+                }
             }
 
         } catch (IOException e) {
 
             e.printStackTrace();
-        }
 
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         //add try/catch to close resources
     }
 
     private void createHash() {
+
         //create relationship between questions and answers
-        pairs = new HashMap<>();
         for (int i=0; i<questions.size();i++) {
             pairs.put(questions.get(i), answers.get(i));
         }
 
-        //for testing:
-        System.out.println(pairs);
     }
 
     private void newQuestion() {
@@ -124,15 +137,17 @@ public class QuizActivity extends Activity {
         currentQuestion = questions.get(0);
 
         ArrayList<String> choices = new ArrayList<>(4);
-        Random rand = new Random(answers.size());
+        Random rand = new Random();
 
         //add the correct answer to available choices
         choices.add(pairs.get(currentQuestion));
 
         //populate the rest of the choices with random answers
         for (int i=0;i<3;i++) {
-            choices.add(answers.get(rand.nextInt()));
+            choices.add(answers.get(rand.nextInt(answers.size())));
         }
+
+        //add validation to make sure all answers are unique
 
         //randomize the order the answers will appear
         Collections.shuffle(choices);
